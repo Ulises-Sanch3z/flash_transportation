fcd562060e02ab7d9df74ee1e5bd3b95d75a465f
Ulises_Sanch3z <chino287a@gmail.com>
2023-12-26 12:19:06 -0500

    added html files

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {'id': 1,
+     'title': 'Data Analyst',
+     'location': 'Delhi, India',
+     'salary': 'Rs. 10,000,000'},
+    {'id': 1,
+     'title': 'Data Analyst',
+     'location': 'Delhi, India',
+     'salary': 'Rs. 10,000,000'},
+    {'id': 1,
+     'title': 'Data Analyst',
+     'location': 'Delhi, India',
+     'salary': 'Rs. 10,000,000'},
+         {'id': 1,
+     'title': 'Cyber Security',
+     'location': 'Remote'}
+]
+
 @app.route("/")
 
-def home():
-    return "Welcome to the Jovian website!!!"
-
+def hello_jovian():
+    return render_template("home.html",
+                            jobs = JOBS,
+                            company_name='Jovian')
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
+
