7a25f8094b40273cdd3403e897baeee5876e516a
Ulises_Sanch3z <chino287a@gmail.com>
2023-12-30 17:16:19 -0500

    added more info to the home page and started working on a form to contact customer

--- app.py
+++ app.py
@@ -2,34 +2,16 @@ from flask import Flask, render_template, url_for, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-    {'id': 1,
-     'title': 'Data Analyst',
-     'location': 'Delhi, India',
-     'salary': 'Rs. 10,000,000'},
-    {'id': 1,
-     'title': 'Data Analyst',
-     'location': 'Delhi, India',
-     'salary': 'Rs. 10,000,000'},
-    {'id': 1,
-     'title': 'Data Analyst',
-     'location': 'Delhi, India',
-     'salary': 'Rs. 10,000,000'},
-         {'id': 1,
-     'title': 'Cyber Security',
-     'location': 'Remote'}
-]
-
 @app.route("/")
 
 def hello_jovian():
     return render_template("home.html",
-                            jobs = JOBS,
                             company_name='Los Niños Flash Transportation')
-@app.route("/jobs")
-def list_jobs():
-    return jsonify(JOBS)
+@app.route("/contact")
+def form():
+    return render_template("contact_us.html")
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
 
+  
