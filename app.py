6dccfcb60ba680e10fbba9d1b13a5af74a5eeee6
Ulises_Sanch3z <chino287a@gmail.com>
2023-12-31 16:27:23 -0500

    added about page

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ def hello_jovian():
 @app.route("/contact")
 def form():
     return render_template("contact_us.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
 
 
-  
+   
